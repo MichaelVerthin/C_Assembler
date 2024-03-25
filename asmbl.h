606ca96ca9a7db448655984b6e8cc7fdf7aabce2
MichaelVerthin <michaelvn5202@gmail.com>
2024-03-25 20:06:40 +0200

    cmt 6.1 | some fixes...

--- asmbl.h
+++ asmbl.h
@@ -71,35 +71,5 @@ union
     line_t *command;
     line_t *macro;
 } line_u_;
-// int first_pass(FILE *fptr)
-// {
-//     char *line = (char *)malloc(sizeof(char) * LINE_LEN);
 
-//     /* Reading line by line of the file*/
-//     while (fgets(line, LINE_LEN, fptr) && line[0] != ';')
-//     {
-//         /* FIRST PASS */
-
-//         /* Is MACRO? */
-//         /**
-//          * TODO - Configure Macro checker
-//          */
-//         /* Is Data Holder? such as array or variable? */
-//         /**
-//          * TODO - Configure array parser
-//          */
-//         /* .extern or .entry ? */
-//         /**
-//          * TODO - Configure .extern/.entry
-//          */
-
-//         fprintf(stdout, "%s", line);
-//     }
-//     return EXIT_SUCCESS;
-// }
-
-// int second_pass(FILE *fptr)
-// {
-//     return EXIT_SUCCESS;
-// }
 #endif
